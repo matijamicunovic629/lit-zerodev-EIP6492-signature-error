c860343a608a2e54c5abe4d09b03fe73a89960a5
matijamicunovic629 <micunovicmatija629@gmail.com>
2025-05-07 14:32:16 +0200

    [Error] it failed when we use bsc instead of sepolia

--- chain-abstraction/regular-validator.ts
+++ chain-abstraction/regular-validator.ts
@@ -5,10 +5,10 @@ import {LitNodeClient} from "@lit-protocol/lit-node-client";
 import {PKPEthersWallet} from "@lit-protocol/pkp-ethers";
 import {LIT_NETWORKS_KEYS} from "@lit-protocol/types";
 
-import {createPublicClient, hashMessage, Hex, http} from "viem"
+import {createPublicClient, hashMessage, Hex, http, isAddress} from "viem"
 import {privateKeyToAccount} from "viem/accounts"
 import {getEntryPoint, KERNEL_V3_1} from "@zerodev/sdk/constants";
-import {sepolia} from "viem/chains";
+import {bsc, sepolia} from "viem/chains";
 import {signerToEcdsaValidator} from "@zerodev/ecdsa-validator";
 import {createKernelAccount, verifyEIP6492Signature} from "@zerodev/sdk";
 import {LIT_ABILITY, LIT_NETWORK, LIT_RPC} from "@lit-protocol/constants";
@@ -22,10 +22,32 @@ const NETWORK = LIT_NETWORK.DatilDev
 
 const SEPOLIA_RPC_URL = 'https://sepolia.infura.io/v3/b6bf7d3508c941499b10025c0776eaf8'
 const PKP_PUB_KEY = '0x04e48499f0f44c505a4275b2b6c83a771ef4579469ac8773daddbc0090f86d6ff9b961a9301b378afb793db3aeebe47ec3b56511f007ede8b7e10e1b3898905e51'
+const PRIVATE_KEY = '0x0b21bb37586bc78fe9feab33efae4e5c11dd97d3aca9d60049cb3abf40f200fa'
 const entryPoint = getEntryPoint("0.7");
 const kernelVersion = KERNEL_V3_1
+/*
 const RPC_URL =
     'https://boldest-lingering-mansion.ethereum-sepolia.quiknode.pro/4e8ff9604fe745a7e3667de33e5e66d196fa5778/';
+*/
+const RPC_URL =
+    'https://bsc-dataseed.binance.org/';
+
+
+const isValidSCWCAIP = (wallet: string): boolean => {
+    try {
+        const walletComponent = wallet.split(':');
+        return (
+            walletComponent.length === 4 &&
+            walletComponent[0] === 'scw' &&
+            walletComponent[1] === 'eip155' &&
+            !isNaN(Number(walletComponent[2])) &&
+            Number(walletComponent[2]) > 0 &&
+            isAddress(walletComponent[3])
+        );
+    } catch (err) {
+        return false;
+    }
+};
 
 
 class KernelSigner extends Signer {
@@ -83,7 +105,7 @@ const main = async () => {
 
     // ------------ begin of connecting pkp wallet with public key --------------
     const ethersWallet = new ethers.Wallet(
-        '0x0b21bb37586bc78fe9feab33efae4e5c11dd97d3aca9d60049cb3abf40f200fa',
+        PRIVATE_KEY,
         new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE)
     );
 
@@ -112,7 +134,7 @@ const main = async () => {
         controllerSessionSigs: pkpSessionSigs
     });
 
-    pkpEthersWallet.setRpc(SEPOLIA_RPC_URL)
+    pkpEthersWallet.setRpc(RPC_URL)
     console.log("pkpEthersWallet address", pkpEthersWallet.address)
     // ------------ end of connecting pkp wallet with public key --------------
 
@@ -149,14 +171,19 @@ const main = async () => {
 
     const publicClient = createPublicClient({
         transport: http(),
-        chain: sepolia,
+        chain: bsc,
     })
+
+    //------------ test code---------------
+    const viemSigner = privateKeyToAccount(PRIVATE_KEY);
+    //------------ test code---------------
 
     // use validator as PKP wallet
     const ecdsaValidatorForPkpWallet = await signerToEcdsaValidator(publicClient, {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        signer: pkpEthersWallet,
+        // signer: pkpEthersWallet,
+        signer: viemSigner,
         entryPoint,
         kernelVersion
     })
@@ -186,11 +213,16 @@ const main = async () => {
 
 // --- initializing pushAPI instance ------------
     const caipAddress = `scw:eip155:${publicClient.chain.id}:${accountForPkpWallet.address}`;
+    const isValid = isValidSCWCAIP(caipAddress);
+    console.log('Is valid CAIP address:', isValid);
+
     const signer1 = new KernelSigner(accountForPkpWallet, new providers.JsonRpcProvider(RPC_URL));
     const userAlice = await PushAPI.initialize(signer1, {
         account: caipAddress,
-        env: CONSTANTS.ENV.PROD,
+        env: CONSTANTS.ENV.STAGING,
     });
+
+    console.log("initalized result: ", userAlice.errors.length > 0 ? 'ERROR' : 'OK!!!')
 // --- end of initializing pushAPI instance ------------
 
     // use validator as EOA wallet
